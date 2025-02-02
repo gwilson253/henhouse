61808da7f4a5313009c808e97dfc1f9deca8d744
gwilson253 <gwilson253@gmail.com>
2025-02-01 22:29:17 -0800

    added data collection

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # henhouse
-Simulation of a hot-dice style game
+Python clone of a dice game called Chicken. 
+
+There is no `requirements.txt` file because no files are used outside of the standard python library. You should be able to run this with Python 3.x. 
+
+The purpose of this project is to use the game API to simulate large numbers of rounds to generate data and identify an optimal strategy. 
+
+# Possible extensions
+* Use as a basis for training a reinforcement model. 
+
--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 import random
 import uuid
 
+from dataclasses import dataclass
+
+DATA_FILE_PATH = 'data.csv'
+
 class Dice():
 
     def __init__(self):
@@ -85,45 +89,87 @@ class DiceSet():
     def count_foxes(self):
         return len([_ for _ in self.dice_set if _.value == 'fox'])
 
+@dataclass
+class TurnRecord:
+    game_id: str = None
+    round: str = None
+    player_id: str = None
+    roll_1_dice_count: int = None # number of die passed to the player at the start of their turn
+    roll_1_reset_dice_set: int = 0 # whether the player reset the dice set at the start of their turn
+    roll_1_chickens: int = None # count of chickens from first roll
+    roll_1_foxes: int = None # count of foxes from first roll
+    roll_1_bust: int = 0 # whether roll 1 was a bust (i.e. 3+ foxes)
+    roll_2_dice_count: int = 0 # number of die in play for the second roll of the turn
+    roll_2_pass: int = 0 # whether the player declines their second roll
+    roll_2_chickens: int = 0 # count of chickens from second roll
+    roll_2_foxes: int = 0 # count of foxes from second roll
+    roll_2_bust: int = 0 # whether roll 1 was a bust (i.e. 3+ foxes)
+    turn_score: int = None # total points earned in the turn
 
-class Hand():
+    def __str__(self):\
+       return ','.join(str(v) if v is not None else '' for v in self.__dict__.values())
 
-    def __init__(self, dice_set: DiceSet):
+class Turn():
+
+    def __init__(self, dice_set: DiceSet, turn_record: TurnRecord):
         self.dice_set = dice_set
+        self.turn_record = turn_record
         self.dice_set.reset_dice_values()
         self.score = 0
 
     def play(self):
-        for turn in range(2):
-            self.dice_set.add_dice()
+        self.turn_record.roll_1_dice_count = self.dice_set.count_dice()
 
-            # possibly reset dice if first turn and more than 4 dice in play
-            if turn == 0 and self.dice_set.count_dice() > 4 and random.choice([False, True]):
-                self.score = -1
-                self.dice_set.reset_dice_set()
+        # r1: decide whether to reset dice set
+        if self.dice_set.count_dice() > 4 and random.choice([False, True]):
+            self.score = -1
+            self.dice_set.reset_dice_set()
+            self.turn_record.roll_1_reset_dice_set = 1
+
+        # r1: roll
+        self.dice_set.roll_dice()
+
+        self.turn_record.roll_1_chickens = self.dice_set.count_chickens()
+        self.turn_record.roll_1_foxes = self.dice_set.count_foxes()
             
-            # roll dice
+        # r1: cash-out decision
+        if (self.dice_set.count_chickens() > 0
+            and self.dice_set.count_foxes() < 3
+            and random.choice([False, True])):
+                self.score += self.dice_set.count_chickens()
+                self.turn_record.roll_2_pass = 1
+
+        # r1: bust?
+        elif self.dice_set.count_foxes() >= 3:
+            self.dice_set.reset_dice_set()
+            self.score = 0
+            self.turn_record.roll_1_bust = 1
+
+        # r2...
+        else:
+            self.dice_set.add_dice()
             self.dice_set.roll_dice()
 
-            # bust
+            self.turn_record.roll_2_dice_count = self.dice_set.count_dice()
+            self.turn_record.roll_2_chickens = self.dice_set.count_chickens()
+            self.turn_record.roll_2_foxes = self.dice_set.count_foxes()
+            
             if self.dice_set.count_foxes() >= 3:
                 self.dice_set.reset_dice_set()
                 self.score = 0
-                break
-            
-            # if score, possibly cash out on first turn
-            if turn == 0 and self.dice_set.count_chickens() > 0 and random.choice([False, True]):
-                break
-
-        self.dice_set.add_dice()
-        self.score += self.dice_set.count_chickens()
+                self.turn_record.roll_2_bust = 1
+            else:
+                self.score += self.dice_set.count_chickens()
+                self.dice_set.add_dice()
+        
+        print(f'Turn Score: {self.score}')
+        self.turn_record.turn_score = self.score
         return self.score
 
+@dataclass
 class Player():
-
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.score = 0
+    id: str = str(uuid.uuid4())
+    score: int = 0
 
 class Game():
 
@@ -138,16 +184,28 @@ class Game():
         while max([_.score for _ in self.players]) < 25:
             print(f'\nRound: {self.round}')
             for player in self.players:
+                tr = TurnRecord(game_id=self.id, round=self.round, player_id=player.id)
                 print(f'\nPlayer ID: {player.id}')
                 print(f'score: {player.score}')
-                hand = Hand(self.dice_set)
-                player.score += hand.play()
+                turn = Turn(self.dice_set, tr)
+                player.score += turn.play()
+                write_turn_record(tr)
                 if player.score >= 25:
                     print(f'Player ID {player.id} wins!')
                     break
-                self.round += 1
+            self.round += 1
 
+def init_data_file():
+    tr = TurnRecord()
+    with open(DATA_FILE_PATH, 'w') as f:
+        f.write(','.join(tr.__dict__.keys())+'\n')
+
+def write_turn_record(turn_record):
+    with open(DATA_FILE_PATH, 'a') as f:
+        f.write(str(turn_record) + '\n')
 
 if __name__ == '__main__':
-    g = Game()
-    g.play()
+    init_data_file()
+    for i in range(5):
+        g = Game()
+        g.play()
